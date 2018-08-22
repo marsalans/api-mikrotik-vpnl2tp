7c6134c7407e15344bde49a23896e63dc618e230
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-08-22 16:23:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,24 @@
 *
 *
 */
+require("lib/api.class.php");
+$API = new routeros_api();
+$API->debug = false;
+
+$username_mikrotik  = "admin";
+$password_mikrotik  = "password";
+$iphost_mikrotik    = "192.168.0.1";
+
+if($API->connect($iphost_mikrotik, $username_mikrotik, $password_mikrotik)){
+  
+  /** This Action To Creat VPN Account **/
+  
+  else {
+  echo " Router Not Connected";
+  }
+
+
+
 
 
 ?>
